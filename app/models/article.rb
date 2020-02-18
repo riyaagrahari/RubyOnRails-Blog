ee888571d680120231aab7bc57d5959cf8083dba
riyaagrahari <500052961@stu.upes.ac.in>
2020-02-19 00:54:36 +0530

    Implement: Delete Commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
     validates :title, presence: true, length: { minimium: 5 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
   resources :articles do
-    resource :comments
+    resources :comments
   end
   root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
