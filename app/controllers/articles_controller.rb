4871311bcbea1d04ea1adfdde8678280cecb8056
riyaagrahari <500052961@stu.upes.ac.in>
2020-02-18 16:26:19 +0530

    Added: Database

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,24 @@
 class ArticlesController < ApplicationController
+    # list all articles
+    def index
+        @articles = Article.all
+    end
+    # show particular article by id
+    def show
+        @article = Article.find(params[:id])
+    end
     def new
     end
-
+    # craete new article
     def create
-        render plain: params[:article].inspect
+        @article = Article.new(article_params)
+
+        @article.save
+        redirect_to @article
     end
+
+    private
+        def article_params
+            params.require(:article).permit(:title,:text)
+        end
 end
